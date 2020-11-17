042213b364b8e3396e5d54fa0527249a1418016a
mahmutsel <mahmutsel29@gmail.com>
2020-11-17 11:20:34 +0000

    user page added

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -13,9 +14,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       <h3>{login}</h3>
 
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           more
-        </a>
+        </Link>
       </div>
     </div>
   );
