8e407ffc5f0fa991486d1639803053c10afbf58f
mahmutsel <mahmutsel29@gmail.com>
2021-01-13 13:24:01 +0000

    Home and NotFound pages created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-
+import NotFound from './components/pages/NotFound'
 import GithubState from './context/github/GithubState';
 import './App.css';
 
@@ -28,18 +27,10 @@ const App = () => {
           <div className='container'>
             <Alert alert={alert} />{' '}
             <Switch>
-              <Route
-                exact
-                path='/'
-                render={(props) => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
+              <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login' component={User} />
+              <Route component = {NotFound}/>
             </Switch>
           </div>
         </div>
